8598ba19d76483a8d4d8d49c32006754dbfe0025
Jacob Fischer <jacob.t.fischer@gmail.com>
2016-04-22 10:30:14 -0500

    Default port is WS port
    no fucks can be given

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var WebSocket = require("ws");
 var parser = new ArgumentParser({description: "Run the JavaScript client with options to connect to a game server. Must provide a game name to play."});
 parser.addArgument(["game"], {action: "store", help: "the name of the game you want to play on the server"});
 parser.addArgument(["-s", "--server"], {action: "store", dest: "server", defaultValue: "127.0.0.1", help: "the url to the server you want to connect to e.g. locahost:3000"});
-parser.addArgument(["-p", "--port"], {action: "store", dest: "port", defaultValue: 3000, help: "the port to connect to on the server. Can be defined on the server arg via server:port"});
+parser.addArgument(["-p", "--port"], {action: "store", dest: "port", defaultValue: 3088, help: "the ws port to connect to on the server. Can be defined on the server arg via server:port"});
 parser.addArgument(["-n", "--name"], {required: true, action: "store", dest: "playerName", help: "the name you want to use as your AI\"s player name"});
 parser.addArgument(["-i", "--index"], {action: "store", dest: "index", help: "the player number you want to be, with 0 being the first player"});
 parser.addArgument(["-w", "--password"], {action: "store", dest: "password", help: "the password required for authentication on official servers"});
@@ -76,7 +76,6 @@ ws.onmessage = function(message) {
     var parsed;
     try {
         parsed = JSON.parse(message.data);
-        console.log("FUCK")
     }
     catch(err) {
         pased = {
